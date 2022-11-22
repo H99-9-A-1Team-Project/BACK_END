b6f4ee50fba49ce34299b4ffe16d8702584ba23c
warmth424 <56920883+warmth424@users.noreply.github.com>
2022-11-22 16:34:11 +0900

    Fix: Comment 패키지 삭제

--- src/main/java/com/example/backend/Comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.Comment.repository;
-
-import com.example.backend.global.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
