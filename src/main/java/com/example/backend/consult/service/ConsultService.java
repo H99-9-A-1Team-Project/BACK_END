6fc783eb9933217168ba52a162f1fcb513f8c2e4
warmth424 <warmth424@gmail.com>
2022-11-22 13:16:26 +0900

    Feat: 상담 답변 관련 API 구현

--- src/main/java/com/example/backend/consult/service/ConsultService.java
+++ src/main/java/com/example/backend/consult/service/ConsultService.java
@@ -1,17 +1,12 @@
 package com.example.backend.consult.service;
 
 import com.example.backend.consult.dto.UserAllConsultResponseDto;
-import com.example.backend.footsteps.repository.FootstepsRepository;
 import com.example.backend.global.config.auth.UserDetailsImpl;
-import com.example.backend.global.entity.AnswerState;
-import com.example.backend.global.entity.Authority;
-import com.example.backend.global.entity.Consult;
+import com.example.backend.global.entity.*;
 import com.example.backend.consult.dto.RegisterConsultDto;
 import com.example.backend.consult.repository.ConsultRepository;
-import com.example.backend.global.entity.FootstepsPost;
 import com.example.backend.global.exception.customexception.common.AccessDeniedException;
 import com.example.backend.global.exception.customexception.user.UserUnauthorizedException;
-import com.example.backend.user.repository.RealtorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +24,7 @@ public class ConsultService {
 
     @Transactional
     public void registerConsult(UserDetailsImpl userDetails, RegisterConsultDto dto) {
+        validAuth(userDetails);
         Consult consult = Consult.builder()
                 .title(dto.getTitle())
                 .coordX(dto.getCoordX())
@@ -42,7 +38,7 @@ public class ConsultService {
                 .consultMessage(dto.getConsultMessage())
                 .createDate(LocalDateTime.now())
                 .user(userDetails.getUser())
-                .answerState(AnswerState.ROLE_WAIT)
+                .answerState(AnswerState.WAIT)
                 .build();
         consultRepository.save(consult);
     }
@@ -57,7 +53,7 @@ public class ConsultService {
 
     public List<UserAllConsultResponseDto> waitConsult(UserDetailsImpl userDetails) {
         validRealtor(userDetails);
-        List<Consult> consultList = consultRepository.findAllByAnswerState(AnswerState.ROLE_WAIT.ordinal());
+        List<Consult> consultList = consultRepository.findAllByAnswerState(AnswerState.WAIT.ordinal());
         return consultList.stream()
                 .map(UserAllConsultResponseDto::new)
                 .collect(Collectors.toList());
@@ -67,10 +63,11 @@ public class ConsultService {
     public void validAuth(UserDetailsImpl userDetails){
         if(userDetails == null) throw new UserUnauthorizedException();
     }
+
     private void validRealtor(UserDetailsImpl userDetails){
+        validAuth(userDetails);
         if(userDetails.getAuthority() != Authority.ROLE_REALTOR)
             throw new AccessDeniedException();
     }
-
 
 }
