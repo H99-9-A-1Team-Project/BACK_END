8ab6a5005e999658d458f79891ef4f22850db82e
kyujae-Jung <skywoolf67@gmail.com>
2022-12-02 20:16:39 +0900

    Feat : 검색 기능

---
+++ src/main/java/com/example/backend/search/service/SearchService.java
@@ -0,0 +1,65 @@
+package com.example.backend.search.service;
+
+import com.example.backend.comment.model.Comment;
+import com.example.backend.consult.model.AnswerState;
+import com.example.backend.consult.model.Consult;
+import com.example.backend.consult.repository.ConsultRepository;
+import com.example.backend.global.security.auth.UserDetailsImpl;
+import com.example.backend.search.dto.MyConsultResponseDto;
+import com.example.backend.user.exception.user.UserUnauthorizedException;
+import com.example.backend.user.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class SearchService {
+
+    private final MemberRepository memberRepository;
+    private final ConsultRepository consultRepository;
+
+    @Transactional
+    public List<MyConsultResponseDto> searchConsult(UserDetailsImpl userDetails, String keyword) {
+        validAuth(userDetails);
+
+        List<Consult> consultList = consultRepository.findAllByUserId(userDetails.getUser().getId());
+        List<MyConsultResponseDto> myConsultResponseDtoList = new ArrayList<>();
+        for (Consult consult : consultList) {
+            if(consult.getAnswerState().equals(AnswerState.WAIT)){
+                myConsultResponseDtoList.add(
+                        MyConsultResponseDto.builder()
+                                .searchWord(keyword)
+                                .consultMessage(consult.getConsultMessage())
+                                .answerState(consult.getAnswerState())
+                                .createDate(consult.getCreateDate())
+                                .title(consult.getTitle())
+                                .build()
+                );
+            }else{
+                myConsultResponseDtoList.add(
+                        MyConsultResponseDto.builder()
+                                .searchWord(keyword)
+                                .comment(consult.getCommentList()
+                                        .stream()
+                                        .map(Comment::getContent)
+                                        .collect(Collectors.toList()).toString())
+                                .answerState(consult.getAnswerState())
+                                .createDate(consult.getCreateDate())
+                                .title(consult.getTitle())
+                                .build()
+                );
+            }
+
+        }
+        return consultRepository.findAllByUserIdAndKeywordContaining(userDetails.getUser().getId(), keyword);
+    }
+
+    public void validAuth(UserDetailsImpl userDetails) {
+        if (userDetails == null) throw new UserUnauthorizedException();
+    }
+}
